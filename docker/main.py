a2a6c853d1f0480dff7d5a6538051baabb3be926
Stephanie-A <albertstephanie630@gmail.com>
2022-04-04 04:59:55 +0100

    : Update main.py

--- docker/main.py
+++ docker/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "<h1>Welcome to SCA Cloud School Application"
+    return "<h1>Welcome to SCA Cloud School Application , this is my first assessment"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
